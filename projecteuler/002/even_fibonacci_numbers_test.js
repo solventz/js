bee4fb5c6ac9d54c7ec152f8bb009ab953f2d582
Greg Tener <borisb0ri5@gmail.com>
2014-05-03 02:48:42 +0000

    Add the second project euler problem.

---
+++ projecteuler/002/even_fibonacci_numbers_test.js
@@ -0,0 +1,30 @@
+define(['chai',
+        'mocha',
+        'projecteuler/002/even_fibonacci_numbers'],
+       function(chai,
+                unusedMocha,
+                evenFibonacciNumbers) {
+  'use strict';
+  var expect = chai.expect;
+  describe('evenFibonacciNumbers', function() {
+    var solutions = {
+      bruteForce: evenFibonacciNumbers.bruteForce
+    };
+    var describeSolution = function(name, solution) {
+      describe(name, function() {
+        it('should solve small input', function() {
+          expect(solution(89)).to.equal(44); 
+        });
+        it('should solve large input', function() {
+          expect(solution(4000000)).to.equal(4613732); 
+        });
+      });
+    };
+    for (var name in solutions) {
+      if (solutions.hasOwnProperty(name)) {
+        var solution = solutions[name];
+        describeSolution(name, solution);
+      }
+    }
+  });
+});
