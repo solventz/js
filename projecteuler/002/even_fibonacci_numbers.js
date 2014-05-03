e775fd7995ae2b24fb337e8b76556d579ea7f2e5
Greg Tener <borisb0ri5@gmail.com>
2014-05-03 21:38:12 +0000

    Add a solution for euler2 that sums every third fibonacci number.

--- projecteuler/002/even_fibonacci_numbers.js
+++ projecteuler/002/even_fibonacci_numbers.js
@@ -14,7 +14,28 @@ define([], function() {
     } while (b < n);
     return sum;
   };
+  var everyThird = function(n) {
+    var a = 1;
+    var b = 2;
+    var sum = 0;
+    do {
+      // Every third fibonacci number is even starting with 2
+      // f_{n+1} = f_n + f_{n-1}
+      // f_{n+2} = f_{n+1} + f_n
+      //         = (f_n + f_{n-1}) + f_n
+      //         = 2f_n + f_{n-1}
+      // f_{n+3} = f_{n+2} + f_{n+1}
+      //         = (2f_n + f_{n-1}) + (f_n + f_{n-1})
+      //         = 3f_n + 2f_{n-1}
+      sum += b;
+      var tmp = b;
+      b = 3 * b + 2 * a;
+      a = 2 * tmp + a;
+    } while (b < n);
+    return sum;
+  };
   return {
     bruteForce: bruteForce,
+    everyThird: everyThird
   };
 });
