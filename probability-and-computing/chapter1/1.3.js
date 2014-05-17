ccde19d41c6105bb05fd244dd8fa090396dbe2bb
Greg Tener <borisb0ri5@gmail.com>
2014-05-17 06:25:33 +0000

    Add the initial setup for exercise 1.3

---
+++ probability-and-computing/chapter1/1.3.js
@@ -0,0 +1,23 @@
+// We shuffle a standard desk of cards, obtaining a permutation that is
+// uniform over all 52! possible permutations. Find the probability of
+// the following events.
+// (a) The first two cards include at least one ace.
+// (b) The first five cards include at least one ace.
+// (c) The first two cards are a pair of the same rank.
+// (d) The first five cards form a full house.
+define([], function() {
+  'use strict';
+  var sampleSpaceSampler = function() {
+  };
+  var predicateA = function(run) {
+  };
+  var predicateB = function(run) {
+  };
+  var predicateC = function(run) {
+  };
+  var predicateD = function(run) {
+  };
+  return {
+    sampleSpaceSampler: sampleSpaceSampler
+  };
+});
---
+++ probability-and-computing/chapter1/1.3_test.js
@@ -0,0 +1,15 @@
+define(['chai',
+        'mocha',
+        'probability-and-computing/chapter1/1.3'],
+       function(chai,
+                unusedMocha,
+                exercise) {
+  'use strict';
+  var expect = chai.expect;
+  describe('probability-and-computing/chapter1/1.3', function() {
+    describe('sampleSpaceSampler', function() {
+      it('generates a permutation of 0..51', function() {
+      });
+    });
+  });
+});
