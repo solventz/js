95b6ebc611e19ce44135047284fb3166e0de0625
Greg Tener <borisb0ri5@gmail.com>
2014-05-22 04:05:11 +0000

    Fix the shuffle method.

--- probability-and-computing/chapter1/1.3.js
+++ probability-and-computing/chapter1/1.3.js
@@ -7,17 +7,74 @@
 // (d) The first five cards form a full house.
 define([], function() {
   'use strict';
-  var sampleSpaceSampler = function() {
+  var shuffle = function(a) {
+    for (var i = 0; i < a.length - 1; i++) {
+      var tmp = a[i];
+      var k = Math.floor(Math.random() * (a.length - i)) + i;
+      a[i] = a[k];
+      a[k] = tmp;
+    }
+    return a;
   };
-  var predicateA = function(run) {
+  var sampleSpaceSampler = function() {
+    var cards = new Array(52);
+    for (var i = 0; i < cards.length; i++) {
+      cards[i] = i;
+    }
+    return shuffle(cards);
+  };
+  var predicateConvergenceVerifier = function(predicate) {
+    return function(expectedProbability) {
+      var positiveSamples = 0;
+      var numCloseConsecutiveSamples = 0;
+      var maxSamples = 1000000;
+      for (var samples = 1; samples <= maxSamples; samples++) {
+        var cards = sampleSpaceSampler();
+        if (predicate(cards)) {
+          positiveSamples += 1;
+        }
+        var observedProbability = positiveSamples / samples;
+        var difference = Math.abs(observedProbability - expectedProbability);
+        if (difference < 0.001 * expectedProbability) {
+          numCloseConsecutiveSamples++;
+        }
+        if (numCloseConsecutiveSamples > 100) {
+          return observedProbability;
+        }
+      }
+      return positiveSamples / maxSamples;
+    };
+  };
+  /*var predicateA = function(run) {
+    return run[0] == 51 || run[1] == 51;
   };
   var predicateB = function(run) {
+    for (var i = 0; i < 5; i++) {
+      if (run[i] == 51) {
+        return true;
+      }
+    }
+    return false;
   };
   var predicateC = function(run) {
-  };
-  var predicateD = function(run) {
+    return run[0] == run[1];
+  };*/
+  var predicateD = function(cards) {
+    var firstCards = {};
+    for (var i = 0; i < 5; i++) {
+      firstCards[cards[i]] = firstCards[cards[i]] + 1 || 1;
+      if (Object.keys(firstCards).length > 2) {
+        return false;
+      }
+      if (firstCards[cards[i]] == 4) {
+        return false;
+      }
+    }
+    return true;
   };
   return {
-    sampleSpaceSampler: sampleSpaceSampler
+    sampleSpaceSampler: sampleSpaceSampler,
+    predicateConvergenceVerifier: predicateConvergenceVerifier,
+    predicateD: predicateD
   };
 });
--- probability-and-computing/chapter1/1.3_test.js
+++ probability-and-computing/chapter1/1.3_test.js
@@ -9,7 +9,45 @@ define(['chai',
   describe('probability-and-computing/chapter1/1.3', function() {
     describe('sampleSpaceSampler', function() {
       it('generates a permutation of 0..51', function() {
+        var cards = exercise.sampleSpaceSampler(); 
+        expect(cards.length).to.equal(52);
+        var seenCards = {};
+        for (var i = 0; i < cards.length; i++) {
+          seenCards[i] = true;
+        }
+        expect(Object.keys(seenCards).length).to.equal(52);
       });
+    });
+    describe('predicateD', function() {
+      it('should return true for full houses', function() {
+        expect(exercise.predicateD([0, 0, 0, 1, 1])).to.equal(true);
+        expect(exercise.predicateD([0, 0, 0, 0, 1])).to.equal(false);
+        expect(exercise.predicateD([0, 1, 0, 0, 1])).to.equal(true);
+      });
+    });
+    describe('predicateConvergenceVerifier', function() {
+      var expectProbabilityConvergesTo = function(verifier, p) {
+        expect(verifier(p)).to.be.within(p - 0.01 * p, p + 0.01 * p);
+      };
+      it('should converge to 0.5', function() {
+        var firstCardIsEven = function(cards) {
+          return cards[0] % 2 === 0;
+        };
+        var firstCardIsEvenVerifier = exercise.predicateConvergenceVerifier(firstCardIsEven);
+        expectProbabilityConvergesTo(firstCardIsEvenVerifier, 1 / 2);
+      });
+      it('should converge to 1 / 52', function() {
+        var firstCardIsAce = function(cards) {
+          return cards[0] == 51;
+        };
+        var firstCardIsAceVerifier = exercise.predicateConvergenceVerifier(firstCardIsAce);
+        expectProbabilityConvergesTo(firstCardIsAceVerifier, 1 / 52);
+      });
+    });
+    it('answers a', function() {
+      // (a) The first two cards include at least one ace.
+ //     var answer = 0.42;
+//      expect(execrise.a(answer)).to.equal(answer);
     });
   });
 });
